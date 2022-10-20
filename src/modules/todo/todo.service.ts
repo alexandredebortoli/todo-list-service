db2d8113ae5542d6e7764b1e05b16fe794ff0891
alexandredebortoli <souza.alexandre@edu.univali.br>
2022-10-20 14:17:11 -0300

    added getById

--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -1,6 +1,7 @@
 import { Todo } from './models/todo.model';
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { HttpException, Inject, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class TodoService {
@@ -12,8 +13,24 @@ export class TodoService {
   private logger = new Logger(TodoService.name);
   
   async getAll() {
+    this.logger.debug('grpc request: get all')
     const todos = await this.todoRepository.findAll();
     return { todos };
+  }
+
+  getById(id: string) {
+    this.logger.debug('grpc request: get by id')
+    try {
+      const todo = this.todoRepository.findByPk(id);
+
+      if (!todo) {
+        throw new RpcException("id not found");
+      }
+
+      return todo;
+    } catch (error) {
+      this.logger.error(error.message);
+    }
   }
 
   createTodo() {
