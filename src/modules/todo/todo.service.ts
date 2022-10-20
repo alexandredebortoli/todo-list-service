8e8091cf90f394b56c38bdb984e3cb7f62e6d340
alexandredebortoli <souza.alexandre@edu.univali.br>
2022-10-20 13:51:04 -0300

    added getAll

--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -1,5 +1,5 @@
 import { Todo } from './models/todo.model';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
@@ -7,7 +7,14 @@ export class TodoService {
   constructor(
     @Inject('TODO_REPOSITORY')
     private todoRepository: typeof Todo
-    ) {}
+  ) {}
+
+  private logger = new Logger(TodoService.name);
+  
+  async getAll() {
+    const todos = await this.todoRepository.findAll();
+    return { todos };
+  }
 
   createTodo() {
     this.todoRepository.create({
